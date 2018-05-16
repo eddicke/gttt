f00fd208c5ddaec98540ab61a549b75ff38f4f1f
eddicke <adenekanademola931@gmail.com>
2018-05-16 19:38:17 -0400

    Update palmfronds.js

--- dist/palmfronds.js
+++ dist/palmfronds.js
@@ -1,11 +1,8 @@
 //PalmFronds
 //Creation of a material with an alpha texture
-var materialSphere15 = new BABYLON.StandardMaterial("texture5", scene);
-materialSphere15.diffuseTexture = new BABYLON.Texture("https://rawcdn.githack.com/eddicke/chat-example/master/assets/textures/d120e4f72fbbc4bde4bac1b768f29af6.png", scene);
-materialSphere15.diffuseTexture.hasAlpha = true; //Has an alpha
-materialSphere15.backFaceCulling = false
 
-var createPalmfrond = function(material) {
+
+var createPalmfrond = function(material, scene) {
     var paths = [];
     for (var t = 1; t < 10; t++) {
         var path = [];
@@ -18,8 +15,12 @@ var createPalmfrond = function(material) {
         paths.push(path);
     }
 
+    var materialSphere15 = new BABYLON.StandardMaterial("texture5", scene);
+materialSphere15.diffuseTexture = new BABYLON.Texture("https://rawcdn.githack.com/eddicke/chat-example/master/assets/textures/d120e4f72fbbc4bde4bac1b768f29af6.png", scene);
+materialSphere15.diffuseTexture.hasAlpha = true; //Has an alpha
+materialSphere15.backFaceCulling = false
+    
     var ribbon = BABYLON.Mesh.CreateRibbon("rib", paths, false, false, 0, scene);
-    ribbon.material = mat;
     ribbon.rotation.x = 28
     var rad = 80.1
     lv = []
@@ -38,24 +39,26 @@ var createPalmfrond = function(material) {
         leaves.position.z = Math.sin(i) * rad
         leaves.rotation = rot[i]
         lv.push(leaves)
-        leaves.material = material;
+        leaves.material = materialSphere15;
     }
     ribbon.setEnabled(false)
 
     var palmfrond = new BABYLON.Mesh.MergeMeshes(lv)
     palmfrond.scaling.set(1 / 2, 2.8 / 2, 1 / 2)
-    return palmfrond;
-}
-
-var leaf = new createPalmfrond(materialSphere15)
-leaf.position.y = 80
-
-var v = []
+    palmfrond.material = materialSphere15
+    palmfrond.position.y = 80
+    var v = []
 for (var k = 0; k < 1000; k++) {
-    var lf = leaf.clone()
+    var lf = palmfrond.clone()
     lf.position.x = Math.random() * 1400 * (k / 4)
     lf.position.z = Math.random() * 1400 * (k / 4)
     v.push(lf)
 }
 
 var tp = new BABYLON.Mesh.MergeMeshes(v)
+return tp;
+}
+
+
+
+
